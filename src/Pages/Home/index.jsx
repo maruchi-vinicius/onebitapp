7a680cc271f61a07b7ca8887159939136534e61d
maruchi-vinicius <maruchi.vinicius@gmail.com>
2023-01-05 10:40:57 -0300

    adicionada barra de status na home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import LifeStatus from "../../Components/Common/LifeStatus";
 import { useNavigation } from "@react-navigation/native"
+import StatusBar from "../../Components/Home/StatusBar";
 
 
 export default function Home(){
@@ -19,6 +20,7 @@ export default function Home(){
                     <Text style={styles.dailyChecks}>
                         20 Dias - 80 Checks
                         <LifeStatus />
+                        <StatusBar />
                     </Text>
                 </View>
 
