041b6eca9e4efa55585ccfef412418de12e03936
maruchi-vinicius <maruchi.vinicius@gmail.com>
2023-01-05 10:47:59 -0300

    corrigido erro de digitação no componente da barra de status

--- src/Components/Home/StatusBar/index.jsx
+++ src/Components/Home/StatusBar/index.jsx
@@ -9,22 +9,22 @@ export default function StatusBar() {
         <View style={styles.container}>
 
             <View style={styles.statusBarContainer}>
-                <Image source={require("../../../assets/icons/educationIcon.png")} style={style.icon} />
+                <Image source={require("../../../assets/icons/educationIcon.png")} style={styles.icon} />
                 <ProgressBar progress={1} color={"#90B7F3"} style={styles.progress} />
             </View>
 
             <View style={styles.statusBarContainer}>
-                <Image source={require("../../../assets/icons/moneyIcon.png")} style={style.icon} />
+                <Image source={require("../../../assets/icons/moneyIcon.png")} style={styles.icon} />
                 <ProgressBar progress={0} color={"#85BB65"} style={styles.progress} />
             </View>
 
             <View style={styles.statusBarContainer}>
-                <Image source={require("../../../assets/icons/bodyIcon.png")} style={style.icon} />
+                <Image source={require("../../../assets/icons/bodyIcon.png")} style={styles.icon} />
                 <ProgressBar progress={0} color={"#FF0043"} style={styles.progress} />
             </View>
 
             <View style={styles.statusBarContainer}>
-                <Image source={require("../../../assets/icons/funIcon.png")} style={style.icon} />
+                <Image source={require("../../../assets/icons/funIcon.png")} style={styles.icon} />
                 <ProgressBar progress={0} color={"#FE7F23"} style={styles.progress} />
             </View>
 
