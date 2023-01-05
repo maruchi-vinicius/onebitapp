ea54b419238a3a7b70c9d0aedfaa57b4d3f923fa
maruchi-vinicius <maruchi.vinicius@gmail.com>
2023-01-05 12:47:57 -0300

    criado botão de adicionar meta

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import LifeStatus from "../../Components/Common/LifeStatus";
 import { useNavigation } from "@react-navigation/native"
 import StatusBar from "../../Components/Home/StatusBar";
+import CreateHabit from "../../Components/Home/CreateHabit";
 
 
 export default function Home(){
     const navigation = useNavigation();
+
+    const [mindHabit, setMindHabit] = useState();
+    const [moneyHabit, setMoneyHabit] = useState();
+    const [bodyHabit, setBodyHabit] = useState();
+    const [funHabit, setFunHabit] = useState();
 
     function handleNavExplanation(){
         navigation.navigate("AppExplanation");
@@ -21,6 +27,7 @@ export default function Home(){
                         20 Dias - 80 Checks
                         <LifeStatus />
                         <StatusBar />
+                        <CreateHabit habitArea={"Mente"} borderColor="#90B7F3"/>
                     </Text>
                 </View>
 
